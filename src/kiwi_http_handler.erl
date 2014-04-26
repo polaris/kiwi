9f47f14f5b4ed1b5450d6e938b40a88d3f1e5259
Jan Deinhard <jan.deinhard@gmail.com>
2014-04-26 08:12:05 +0200

    add support for JSON

--- src/kiwi_http_handler.erl
+++ src/kiwi_http_handler.erl
@@ -3,7 +3,7 @@
 -export([init/3]).
 -export([content_types_provided/2]).
 -export([value_to_text/2]).
-% -export([hello_to_json/2]).
+-export([value_to_json/2]).
 
 init(_Transport, _Req, []) ->
   {upgrade, protocol, cowboy_rest}.
@@ -11,20 +11,21 @@ init(_Transport, _Req, []) ->
 content_types_provided(Req, State) ->
   {[
     {<<"text/plain">>, value_to_text},
-    {<<"text/html">>, value_to_text}
-    % {<<"application/json">>, hello_to_json}
+    {<<"text/html">>, value_to_text},
+    {<<"application/json">>, value_to_json}
   ], Req, State}.
 
 value_to_text(Req, State) ->
-  {Body, Req2} = get_value(Req),
+  {_Key, Body, Req2} = get_value(Req),
   {Body, Req2, State}.
 
-% hello_to_json(Req, State) ->
-%   Body = <<"{\"rest\": \"Hello World!\"}">>,
-%   {Body, Req, State}.
+value_to_json(Req, State) ->
+  {Key, Value, Req2} = get_value(Req),
+  Body = jiffy:encode({[{list_to_binary(Key), list_to_binary(Value)}]}),
+  {Body, Req2, State}.
 
 get_value(Req) ->
   {Value, Req2} = cowboy_req:binding(key, Req),
   Key = binary_to_list(Value),
   {ok, Val} = kiwi_server:lookup(Key),
-  {Val, Req2}.
+  {Key, Val, Req2}.
